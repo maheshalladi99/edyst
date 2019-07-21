0433a5f25d50afb2b09b3dc6a3562f836e3eee68
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-07-21 13:58:06 +0530

    Update pascals.c

--- pascals.c
+++ pascals.c
@@ -3,8 +3,7 @@ struct Solution {
       /* write your solution here */
        int coef = 1, space, i, j;
      for(i=0; i<rows; i++)
-     {
-       
+     {     
      for(space=1; space <= rows-i; space++)
             cout<<"";
 
