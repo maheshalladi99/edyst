5d01018ad757c725fc388acedf6f96f6ca7f3253
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-07-23 22:51:29 +0530

    Update addonetonum.py

--- addonetonum.py
+++ addonetonum.py
@@ -2,8 +2,6 @@ class Solution:
     def plusOne(self, A):
         s = [str(i) for i in A]
         res = int("".join(s))
-        
-        
         return(list(str(res+1)))
         
         # write your method here
