02b2a8430fcb867270508d246ceed94af041f935
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-11-11 11:10:54 +0530

    Create pointers_array3pointers.py

---
+++ pointers_array3pointers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimize(self, A, B, C):
+        i = len(A) - 1
+        j = len(B) - 1
+        k = len(C) - 1
+        min_diff = abs(max(A[i], B[j], C[k]) -
+        min(A[i], B[j], C[k]))   
+        while i != -1 and j != -1 and k != -1: 
+            current_diff = abs(max(A[i], B[j],  
+            C[k]) - min(A[i], B[j], C[k])) 
+            if current_diff < min_diff: 
+                min_diff = current_diff 
+            max_term = max(A[i], B[j], C[k]) 
+            if A[i] == max_term: 
+                i -= 1
+            elif B[j] == max_term: 
+                j -= 1
+            else: 
+                k -= 1
+        return min_diff 
+    # write your method here
